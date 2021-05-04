8ad0d7463b84fa1275d4615ff752dd78293adc8e
Bhavuk <bhavuksharma2202@gmail.com>
2021-05-05 00:09:21 +0530

    blocks with status signal and clicks working

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
+//	{"",       	"~/scripts/status/speed.sh",				1,			0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", 		"~/scripts/status/dwm_resources.sh",		5,			0},
+
+	{"",		"~/scripts/status/dwm_alsa.sh",				0,			12},
+
+	{"",		"~/scripts/status/dwm_date.sh",				1,			0},
+
+	{"",		"~/scripts/status/dwm_battery.sh",			1000,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
